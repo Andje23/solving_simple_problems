fdf99d45f87ce9fa6b5908843c5635cd628a27a4
Andje23 <andje23@gmail.com>
2022-10-11 23:00:21 +0300

    stream object to get data from microphone

--- audio_visualization_tool.py
+++ audio_visualization_tool.py
@@ -22,3 +22,20 @@ num_devices = info.get('deviceCount')
 for i in range(0, num_devices):
     if(pya.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
         print("Input Device id", i, " - ", pya.get_device_info_by_host_api_device_index(0, 1).get('name'))
+
+audio_input = input("\n\nSelect input by Device id: ")
+
+while not audio_input.isdigit():
+    print("You didn't enter a number.")
+    audio_input = input("\n\nSelect input by Device id: ")
+
+# stream object to get data from microphone
+stream = pya.open(
+    input_device_index=int(audio_input),
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK,
+)
