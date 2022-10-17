250bb9a150c816eda1351c1414155646783d2b33
Andje23 <andje23@gmail.com>
2022-10-17 23:20:58 +0300

    create choose_size_button

--- paint.py
+++ paint.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Button, HORIZONTAL
 
 
 class Paint:
@@ -14,10 +14,13 @@ class Paint:
         self.brush_button: Button = Button(self.root, text='brush', command=self.use_brush)
         self.brush_button.grid(row=0, column=1)
 
-        self.color_button: Button = Button(self.root, text='brush', command=self.choose_color)
+        self.color_button: Button = Button(self.root, text='color', command=self.choose_color)
         self.color_button.grid(row=0, column=2)
 
-        self.eraser_button: Button = Button(self.root, text='brush', command=self.use_eraser)
+        self.eraser_button: Button = Button(self.root, text='eraser', command=self.use_eraser)
         self.eraser_button.grid(row=0, column=3)
+
+        self.choose_size_button: Button = Button(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=4)
 
 
